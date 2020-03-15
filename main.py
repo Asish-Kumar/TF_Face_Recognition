8c925fd55227382f6c78ebeb221533f285ba1ba6
Asish Kumar <asishkumar494@gmail.com>
2020-03-15 20:04:53 +0530

    Fixed a minor bug with GPU.

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from classifier import Classifier
 face_recognition_obj = FaceRecognitionTf()
 classifier_obj = Classifier()
 
-model = load_model('facenet_keras.h5')
+model = load_model('facenet_keras.h5', compile=False)
 
 capture_video = cv2.VideoCapture(0) # 0 to capture video from camera 0
 frame_found, frame = capture_video.read()
@@ -42,7 +42,7 @@ while frame_found:
 
         predicted_name, confidence = classifier_obj.classify(face, embadding)
 
-        cv2.rectangle(frame, (x1, y1), (x2, y2), 20)
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (36, 255, 12))
         cv2.putText(frame, '%s (%.2f%%)'%(predicted_name, confidence), (x1, y1-10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (36, 255, 12), 1)
 
     cv2.imshow("frame", frame)
